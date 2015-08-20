72f6646817e63fd13b345e48a592f983f1a44432
yiren.goo <yiren.goo@gmail.com>
2015-08-20 17:10:34 +0800

    Create Change Request Code First DbContext

---
+++ LungmenSoftware/Models/CodeFirst/ChangeProcessDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LungmenSoftware.Models.CodeFirst
+{
+    public class ChangeProcessDbContext:DbContext
+    {
+        public ChangeProcessDbContext()
+            :base("name=ChangeProcessData")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ LungmenSoftware/Models/CodeFirst/Entities/ChangeRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LungmenSoftware.Models.CodeFirst.Entities
+{
+    public class ChangeRequest
+    {
+
+    }
+}
