682c76402d17673c97c6317908b35d929592e6dd
yiren.goo <yiren.goo@gmail.com>
2015-08-25 14:17:41 +0800

    ChanageRequest V1

--- LungmenSoftware/Models/CodeFirst/Entities/ChangeRequest.cs
+++ LungmenSoftware/Models/CodeFirst/Entities/ChangeRequest.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace LungmenSoftware.Models.CodeFirst.Entities
 {
     public class ChangeRequest
     {
+        //Business Columns
+        public Guid ChangeRequestId { get; set; }
+        public string SerialNumber { get; set; }
+
+        //Status Pattern
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        public DateTime? CommentDate { get; set; }
+
+        public DateTime? ApprovedDate { get; set; }
+        
+        public DateTime? CompletedDate { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
 
     }
 }
