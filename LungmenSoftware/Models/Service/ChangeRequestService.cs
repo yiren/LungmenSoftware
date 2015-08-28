e1dfa40d717ab739cdb0c574809f3716ddf1286b
yiren.goo <yiren.goo@gmail.com>
2015-08-28 08:24:30 +0800

    Successfully Restore Code-First Data

---
+++ LungmenSoftware/Models/Service/ChangeRequestService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LungmenSoftware.Models.CodeFirst;
+using LungmenSoftware.Models.CodeFirst.Entities;
+using LungmenSoftware.Models.ViewModel;
+
+namespace LungmenSoftware.Models.Service
+{
+    public class ChangeRequestService
+    {
+        private ChangeProcessDbContext db=new ChangeProcessDbContext();
+
+        public List<ChangeRequestInfo> GetChangeRequestList()
+        {
+            var query = from cr in db.ChangeRequests
+                join s in db.ChangeRequestStatuses
+                    on cr.ChangeRequestId equals s.ChangeRequestId
+                join t in db.ChangeRequestStatusTypes
+                    on s.StatusTypeId equals t.StatusTypeId
+                select new ChangeRequestInfo
+                {
+                    ChangeRequestId = cr.ChangeRequestId,
+                    ApprovedBy = cr.ApprovedBy,
+                    CreatedBy = cr.CreatedBy,
+                    LastModifiedDate = cr.LastModifiedDate,
+                    ReviewBy = cr.ReviewBy,
+                    SerialNumber = cr.SerialNumber,
+                    ChangeDate = s.ChangeDate,
+                    EndDate = s.EndDate,
+                    InitialDate = s.InitialDate,
+                    StatusName = t.StatusName,
+                    CreateDate = cr.CreateDate
+                };
+            return query.ToList();
+        } 
+
+        public List<ChangeRequestStatusType> GetChangeRequestStatusTypess()
+        {
+            return db.ChangeRequestStatusTypes.ToList();
+        }
+
+    }
+
+   
+}
