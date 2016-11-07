66dbfc47e3e55fa8a09578dff097227933861e38
yiren <joombuopre@outlook.com>
2016-11-07 16:56:51 +0800

    update LungmenSCM link

---
+++ LungmenSoftware/Controllers/WebAPI/ModuleBoardsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LungmenSoftware.Models.V2;
+
+namespace LungmenSoftware.Controllers.WebAPI
+{
+    public class ModuleBoardsController : ApiController
+    {
+        private NumacDbContext db = new NumacDbContext();
+
+        // GET: api/ModuleBoards
+        public IQueryable<ModuleBoard> GetModuleBoards()
+        {
+            return db.ModuleBoards;
+        }
+
+        // GET: api/ModuleBoards/5
+        [ResponseType(typeof(ModuleBoard))]
+        public IHttpActionResult GetModuleBoard(Guid id)
+        {
+            ModuleBoard moduleBoard = db.ModuleBoards.Find(id);
+            if (moduleBoard == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(moduleBoard);
+        }
+
+        // PUT: api/ModuleBoards/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutModuleBoard(Guid id, ModuleBoard moduleBoard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != moduleBoard.ModuleBoardId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(moduleBoard).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModuleBoardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ModuleBoards
+        [ResponseType(typeof(ModuleBoard))]
+        public IHttpActionResult PostModuleBoard(ModuleBoard moduleBoard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ModuleBoards.Add(moduleBoard);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ModuleBoardExists(moduleBoard.ModuleBoardId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = moduleBoard.ModuleBoardId }, moduleBoard);
+        }
+
+        // DELETE: api/ModuleBoards/5
+        [ResponseType(typeof(ModuleBoard))]
+        public IHttpActionResult DeleteModuleBoard(Guid id)
+        {
+            ModuleBoard moduleBoard = db.ModuleBoards.Find(id);
+            if (moduleBoard == null)
+            {
+                return NotFound();
+            }
+
+            db.ModuleBoards.Remove(moduleBoard);
+            db.SaveChanges();
+
+            return Ok(moduleBoard);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ModuleBoardExists(Guid id)
+        {
+            return db.ModuleBoards.Count(e => e.ModuleBoardId == id) > 0;
+        }
+    }
+}
---
+++ LungmenSoftware/client/numacRouter.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("numacApp")
+        
+        .config(
+                function ($stateProvider, $urlRouterProvider) {
+
+                    
+                    $urlRouterProvider.otherwise('/');
+                    
+                    $stateProvider
+                        //Get Numac Change Form
+                        .state('form', {
+                            url: '/',
+                            templateUrl: '/../client/templates/numac/numacChangeForm.html',
+                            controller: 'numacFormCtrl',
+                            controllerAs:'f'
+                        })
+                        //
+                        .state('numacConfirmForm', {
+                            url: '/numacConfirmForm',
+                            templateUrl: '/../client/templates/numac/confirmresponse.html',
+                            controller:'numacConfirmFormCtrl',
+                            controllerAs:'c',
+                            // resolve: {
+                            //     numacFormData:['numacDataService', function(numacDataService){
+                            //         return numacDataService.getNumacChangeRequestPromise();
+                            //     } ]
+                            //     }
+                            resovle:{
+                                numacDataForm:function(){
+                                    // return numacDataService.getNumacChangeRequestPromise().then(
+                                    //     function(res){
+                                    //         return res.data;
+                                    //     },function(err){
+
+                                    //     }
+                                    // );
+                                   return {value:"Hello"};
+                                }
+                            }
+                            }
+                        )
+                        
+                    ;
+                })
+
+})();
